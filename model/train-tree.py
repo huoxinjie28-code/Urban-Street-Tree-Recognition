d533108e4d89131076f50514a1a5786697d927b8
huoxinjie28-code <huoxinjie28@gmail.com>
2025-10-27 18:21:13 +0800

    Update train-tree.py

--- model/train-tree.py
+++ model/train-tree.py
@@ -1,5 +1,5 @@
 
 from ultralytics import YOLO
 
-model = YOLO("D:/ultralytics-main/yolo11n.pt")  # 加载模型
+model = YOLO("D:/ultralytics-main/yolo11n.pt")  
 results = model.train(data="D:/ultralytics-main/tree.yaml", epochs=20, imgsz=640)
